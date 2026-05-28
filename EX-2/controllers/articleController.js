5541662be8947bf708b852e8e91673cd53d2e5ab
Zhiangtong Him <zhiangtong.him@student.cadt.edu.kh>
2026-05-28 10:39:52 +0700

    w04 (2)

--- EX-2/controllers/articleController.js
+++ EX-2/controllers/articleController.js
@@ -1,49 +1,52 @@
-import * as data from "../models/data.js"
+import {articles, categories, journalists} from "../models/data.js"
 
 export const getAllArticle = (req, res) => {
-    res.json(data.articles)
+    res.json(articles)
 }
 
 export const getArticleByID = (req,res) => {
     const articleID = parseInt(req.params.id)
-    const article = data.articles.find( a => a.id === articleID)
+    const article = articles.find( a => a.id === articleID)
     if (!articleID) return res.status(404).json({error : "Article not found!"})
     res.status(200).json(article)
 
 }
 
 export const createArticle = (req, res) => {
-    const {title, content} = req.body
+    const {title, content, journalistID, categoryID} = req.body
     if (!title || !content) return res.status(400).json({error: "Title and content required"})
     
     const newArticle = {
-        id: data.articles.length + 1,
+        id: articles.length + 1,
         "title": title,
         "content": content,
-        "journalistId" : data.articles.journalistId.length + 1,
-        "categoryId" : data.articles.categoryId.length + 1 
+        "journalistId" : journalistID,
+        "categoryId" : categoryID 
     }
-    data.articles.push(newArticle)
+    articles.push(newArticle)
     res.status(201).json(newArticle)
 }
 
 export const updateArticleID = (req, res) => {
     const articleID =parseInt(req.params.id)
-    const {title, content} = req.body   
+    const {title, content,journalistID, categoryID} = req.body   
 
-    const article = data.articles.find( a => a.id === articleID)
+    const article = articles.find( a => a.id === articleID)
     if(!article) return res.status(404).json({error : "Article ID not founded"})
     
     if(title) article.title = title
     if(content) article.content = content
+    if( journalistID === journalists.id ) article.journalistId = journalistID
+    if( categoryID === categories.id ) article.categoryId = categoryID
+
     res.status(200).json(article)
 }
 
 export const deleteArticleByID = (req, res) => {
     const articleID = parseInt(req.params.id)
-    const index = data.articles.findIndex( a => a.id === articleID)
+    const index = articles.findIndex( a => a.id === articleID)
     if(index === -1) return res.status(404).json({error : "Article not found"})
     
-    data.articles.splice(index, 1)
+    articles.splice(index, 1)
     res.status(200).send('Deleted')
 }
